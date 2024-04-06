d626cb50279fe34a768253b7cda3ba4e5de1b8ff
Angus <angushf@students.ubc.ca>
2024-04-06 13:39:56 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const axios = require("axios")
+const PORT = process.env.PORT || 3030;
+
+const apiKey = 'XJldLIIAPnq69757yUCA'; // Replace with your TransLink API key
+const busStopNumber = '61890'; // Replace with your bus stop number
+const url = `http://api.translink.ca/rttiapi/v1/stops/${busStopNumber}/estimates?apikey=${apiKey}&count=3&timeframe=120&routeNo=181`;
+
+// middleware
+app.use(express.json())
+
+
+
+
+app.get('/', (req, res) => {
+    axios.get(url, {
+        headers: {
+            'Accept': 'application/JSON'
+        }
+    })
+    .then(response => {
+        // Let's log the full data to inspect its structure first
+        res.send(response.data[0].Schedules[0]);
+
+    })
+    .catch(error => {
+        console.error('Error fetching data: ', error);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
